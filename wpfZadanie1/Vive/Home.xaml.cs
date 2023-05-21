7670ccf08845cafbbb7e824de87f070ca944558c
Gospodin <galuza_a@list.ru>
2023-05-22 01:45:59 +0300

    Добавьте файлы проекта.

---
+++ wpfZadanie1/Vive/Home.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfZadanie1.Vive
+{
+    /// <summary>
+    /// Логика взаимодействия для Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+            MainFrame.Content = new LogIN();
+        }
+        private void Button_Click_LogIn(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new LogIN();
+            Brush colorTrue = new SolidColorBrush(Color.FromRgb(210, 210, 210));
+            Brush colorFalse = new SolidColorBrush(Color.FromRgb(134, 134, 134));
+            ButtonLogIn.Foreground = colorTrue;
+            ButtonSingUp.Foreground = colorFalse;
+
+        }
+
+        private void Button_Click_SingUP(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new SingUp();
+            Brush colorTrue = new SolidColorBrush(Color.FromRgb(210, 210, 210));
+            Brush colorFalse = new SolidColorBrush(Color.FromRgb(134, 134, 134));
+            ButtonLogIn.Foreground = colorFalse;
+            ButtonSingUp.Foreground = colorTrue;
+        }
+
+    }
+}
---
+++ wpfZadanie1/Vive/SingUp.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace wpfZadanie1
+{
+    /// <summary>
+    /// Логика взаимодействия для SingUp.xaml
+    /// </summary>
+    public partial class SingUp : Page
+    {
+        string connStr = "server=127.0.0.1;user=root;database=zadanie4;";
+        public SingUp()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (password1.Password != password2.Password) { MessageBox.Show("Пароли не совпадают"); return;}
+            if (password1.Password == string.Empty || password2.Password == string.Empty) { MessageBox.Show("Пароли пусты"); return; }
+            if (EmailBox.Text == string.Empty) { MessageBox.Show("Email пуст"); return; }
+            if (LoginBox.Text == string.Empty) { MessageBox.Show("Login пуст"); return; }
+
+            string login = LoginBox.Text;
+            string email=EmailBox.Text;
+            string password=password1.Password;
+
+            // не  доделал 
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+            string sql = "";
+            MySqlCommand command = new MySqlCommand(sql,conn);
+            string result = command.ExecuteScalar().ToString();
+             
+        }
+    }
+}
